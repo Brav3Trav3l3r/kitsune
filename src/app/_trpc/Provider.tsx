debb3a6aafb7682ed69df62f245b53938fa34347
Brav3Trav3l3r <yashgajbhiye10@gmail.com>
2024-04-11 21:56:51 +0530

    adjust base url base on the env

--- src/app/_trpc/Provider.tsx
+++ src/app/_trpc/Provider.tsx
@@ -6,12 +6,26 @@ import { ReactNode, useState } from "react";
 import { trpc } from "./client";
 
 export default function TRPCProvider({ children }: { children: ReactNode }) {
+  function getBaseUrl() {
+    if (typeof window !== "undefined")
+      // browser should use relative path
+      return "";
+    if (process.env.VERCEL_URL)
+      // reference for vercel.com
+      return `https://${process.env.VERCEL_URL}`;
+    if (process.env.RENDER_INTERNAL_HOSTNAME)
+      // reference for render.com
+      return `http://${process.env.RENDER_INTERNAL_HOSTNAME}:${process.env.PORT}`;
+    // assume localhost
+    return `http://localhost:${process.env.PORT ?? 3000}`;
+  }
+
   const [queryClient] = useState(() => new QueryClient());
   const [trpcClient] = useState(() =>
     trpc.createClient({
       links: [
         httpBatchLink({
-          url: "http://localhost:3000/api/trpc",
+          url: `${getBaseUrl()}/api/trpc`,
           // You can pass any HTTP headers you wish here
         }),
       ],
